b501ddc3f643f4750cb10d80b1cfec726ea345b6
Momo Kamal <kamal.guidadou>
2022-12-12 01:00:04 +0100

    Added creation post

--- test-admin/src/App.jsx
+++ test-admin/src/App.jsx
@@ -1,13 +1,14 @@
-import { Admin, Resource, ListGuesser } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import { PostList, PostEdit, PostCreate } from "./posts";
 import { UserList } from "./users";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={UserList} />
-    <Resource name="posts" list={ListGuesser} />
+    <Resource name="users" list={UserList} recordRepresentation="name" />
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
   </Admin>
   );
 
---
+++ test-admin/src/posts.jsx
@@ -0,0 +1,45 @@
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    ReferenceInput,
+    TextInput,
+  } from "react-admin";
+  
+
+export const PostList = () => (
+  <List>
+    <Datagrid>
+      <TextField source="id" />
+      <ReferenceField source="userId" reference="users" />
+      <TextField source="title" />
+      <EditButton />
+    </Datagrid>
+  </List>   
+);
+
+export const PostEdit = () => (
+    <Edit>
+      <SimpleForm>
+      <TextInput source="id" disabled />
+        <ReferenceInput source="userId" reference="users" />
+        <TextInput source="title" />
+        <TextInput source="body" multiline rows={5} />
+      </SimpleForm>
+    </Edit>
+  );
+
+export const PostCreate = () => (
+    <Create>
+      <SimpleForm>
+        <ReferenceInput source="userId" reference="users" />
+        <TextInput source="title" />
+        <TextInput source="body" multiline rows={5} />
+      </SimpleForm>
+    </Create>
+  );
